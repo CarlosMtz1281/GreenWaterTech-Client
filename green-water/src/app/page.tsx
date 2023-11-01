c0cc6b0150b1fec78b47b4dd157dbfd63b4a4362
Carlos Martinez <carlosh.mtz@hotmail.com>
2023-11-01 12:04:16 -0600

    [React][Nav]: NavBar added

--- green-water/src/app/page.tsx
+++ green-water/src/app/page.tsx
@@ -1,12 +1,12 @@
 import Image from 'next/image'
 
+import NavBar from './NavBar/page'
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-
+    <main>
+        <NavBar />
         <h1>Green Water Tech</h1>
-      </div>
-    </main>
+   </main>
   )
 }
