8453fb8cf7e4e4cdfb1ec8bfafcee00896ad5db7
Marco Lucio <marcoalucio15@gmail.com>
2023-11-08 11:49:51 -0600

    Added test route.

---
+++ green-water/src/app/test/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from '../NavBar/page';
+
+const TestPage = () => {
+  return (
+    
+    <div>
+        <NavBar/>
+      <p>If you can see this, the route is working!</p>
+    </div>
+  );
+};
+
+export default TestPage;
