953a17361ea1f44b1b9cdcbb5e608486e36cb83c
Fernando Perez <ferpega03@gmail.com>
2023-11-10 20:13:04 -0600

    format strings

--- Backend/back.py
+++ Backend/back.py
@@ -5,9 +5,12 @@ from firebase_admin import credentials, db
 #Recuperar las credenciales del firebase
 creds = credentials.Certificate('serviceAccCreds.json')
 
+#link de la base de datos
+url = 'https://proyecto-iot-2020.firebaseio.com/'
+
 #Iniciar la app con los permisos de la cuenta
 firebase_admin.initialize_app(creds, {
-    'databaseURL': 'https://proyecto-iot-2020.firebaseio.com/' #Aqui va el url del firebase que tiene toda la info general
+    'databaseURL': 'url' #Aqui va el url del firebase que tiene toda la info general
 })
 
 # De alguna manera obtener el key del user que esta usando la app para de esta manera mandar ese prop como parametro para los urls 
@@ -15,5 +18,18 @@ user = 'userKey'
 
 
 #Leer los datos
-data = db.reference('userKey/campos') #Aqui va el url que da el json de lo que quiero obtener
+data = db.reference(f'{url}/{user}/campos') #Aqui va el url que da el json de lo que quiero obtener
 
+
+
+
+
+
+
+def get_campos(user):
+    data = db.reference(f'{url}/{user}/campos')
+    return data
+
+def get_cuadrantes(user):
+    data = db.reference(f'{url}/{user}/cuadrantes')
+    return data   
