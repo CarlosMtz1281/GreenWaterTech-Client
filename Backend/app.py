0b4d4ba7dc4cbbd7737efd535e8c9748e3ad6e32
Fernando Perez <ferpega03@gmail.com>
2023-11-11 13:42:30 -0600

    Funcion para info de usuario

--- Backend/app.py
+++ Backend/app.py
@@ -1,11 +1,19 @@
 from flask import Flask, request
 import os
-from back import getCampos, getCuadrantes
+from back import getCampos, getCuadrantes, getUserInfo
 
 app = Flask(__name__)
 
+@app.route('/api/getUser', methods=['GET'])
+def get_user():
+    #if request.is_json
+    user = request.args.get('user')
+    response = getUserInfo(user)
+    return response
+
 @app.route('/api/getCampos', methods=['GET'])
 def get_campos():
+    #if request.is_json
     user = request.args.get('user')
     campo = request.args.get('campo')
     response = getCampos(user, campo)
@@ -13,6 +21,7 @@ def get_campos():
 
 @app.route('/api/getCuadrantes', methods=['GET'])
 def get_cuadrantes():
+    #if request.is_json
     user = request.args.get('user')
     campo = request.args.get('campo')
     cuadrante = request.args.get('cuadrante')
--- Backend/back.py
+++ Backend/back.py
@@ -13,14 +13,34 @@ firebase_admin.initialize_app(creds, {
     'databaseURL': url #Aqui va el url del firebase que tiene toda la info general
 })
 
+userGlobal = ""
+
+#Funciones para cambiar base de datos
+
+
+
+
+
 #Funciones para obtener los datos de la base de datos
-def getCampos(user, campo):
-    data = db.reference(f'Users/{user}/{campo}')
+def getUserInfo(user):
+    data = db.reference(f'Users/{user}')
+    userGlobal = user
     return data.get()
 
-def getCuadrantes(user, campo, cuadrante):
-    data = db.reference(f'User/{user}/{campo}/{cuadrante}')
+def getCampos(campo):
+    data = db.reference(f'Users/{userGlobal}/{campo}')
     return data.get()
 
+def getCuadrantes(campo, cuadrante):
+    data = db.reference(f'User/{userGlobal}/{campo}/{cuadrante}')
+    return data.get()
+
+
+#Pruebas
 data = getCampos('-NizbbRQkf0AqfSdZIfv', 'campo1')
 print(data)
+
+print("\n")
+
+data2 = getUserInfo('-NizbbRQkf0AqfSdZIfv')
+print(data2)
