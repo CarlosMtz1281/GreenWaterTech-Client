e5b87847b2505cac0709109f171e113a18198622
Fernando Perez <ferpega03@gmail.com>
2023-11-11 12:55:51 -0600

    Funciona chido, checar llamadas front

--- Backend/app.py
+++ Backend/app.py
@@ -1,11 +1,23 @@
-from flask import Flask
+from flask import Flask, request
 import os
+from back import getCampos, getCuadrantes
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Hello World!'
+@app.route('/api/getCampos', methods=['GET'])
+def get_campos():
+    user = request.args.get('user')
+    campo = request.args.get('campo')
+    response = getCampos(user, campo)
+    return response
+
+@app.route('/api/getCuadrantes', methods=['GET'])
+def get_cuadrantes():
+    user = request.args.get('user')
+    campo = request.args.get('campo')
+    cuadrante = request.args.get('cuadrante')
+    response = getCuadrantes(user, campo, cuadrante)
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True, host="3000")
--- Backend/back.py
+++ Backend/back.py
@@ -6,30 +6,21 @@ from firebase_admin import credentials, db
 creds = credentials.Certificate('serviceAccCreds.json')
 
 #link de la base de datos
-url = 'https://proyecto-iot-2020.firebaseio.com/'
+url = 'https://greenwatertech-572bc-default-rtdb.firebaseio.com/'
 
 #Iniciar la app con los permisos de la cuenta
 firebase_admin.initialize_app(creds, {
-    'databaseURL': 'url' #Aqui va el url del firebase que tiene toda la info general
+    'databaseURL': url #Aqui va el url del firebase que tiene toda la info general
 })
 
-# De alguna manera obtener el key del user que esta usando la app para de esta manera mandar ese prop como parametro para los urls 
-user = 'userKey'
+#Funciones para obtener los datos de la base de datos
+def getCampos(user, campo):
+    data = db.reference(f'Users/{user}/{campo}')
+    return data.get()
 
+def getCuadrantes(user, campo, cuadrante):
+    data = db.reference(f'User/{user}/{campo}/{cuadrante}')
+    return data.get()
 
-#Leer los datos
-data = db.reference(f'{url}/{user}/campos') #Aqui va el url que da el json de lo que quiero obtener
-
-
-
-
-
-
-
-def get_campos(user):
-    data = db.reference(f'{url}/{user}/campos')
-    return data
-
-def get_cuadrantes(user):
-    data = db.reference(f'{url}/{user}/cuadrantes')
-    return data   
+data = getCampos('-NizbbRQkf0AqfSdZIfv', 'campo1')
+print(data)
